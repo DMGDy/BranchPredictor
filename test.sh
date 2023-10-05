7cee017670071eb093ad915b757a807057e676ab
DMGDy <dylangarza1909@gmail.com>
2023-10-05 02:46:30 -0400

    ease of testing program

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./Branch.Grp1 branches 4 10 2
+#./Branch.Grp1 branches 6 10 2
+#./Branch.Grp1 branches 4 14 3
+#./Branch.Grp1 branches 2 6 1
+#./Branch.Grp1 branches 6 10 1
+#./Branch.Grp1 branches 6 6 3
+
+
