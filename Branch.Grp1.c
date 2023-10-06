4bfe3e0242a4bc8911c6a7be81698ae4de7a0ddb
DMGDy <dylangarza1909@gmail.com>
2023-10-06 00:45:25 -0400

    almost

--- Branch.Grp1.c
+++ Branch.Grp1.c
@@ -60,11 +60,14 @@ typedef struct
 
 char* concat(const char*,const char*);
 void print_results(branch_stats*);
-void increment_entries(branch_stats*);
 branch_stats* init_branch_stats(void);
 GAp_predictor* init_GAp_predictor(unsigned,unsigned,unsigned);
 void analyse_trace(GAp_predictor*,branch_stats,char*);
+int check_prediction(GAp_predictor*,unsigned);
+void update_branch_predictor(GAp_predictor*,int,int,unsigned);
+void update_statistics(branch_stats*,int);
 unsigned extract_PC_bits(unsigned,uint32_t);
+
 /**
  * Concatenates 2 string into 1
  *
@@ -92,17 +95,29 @@ init_branch_stats(void)
     stats->reads = 0;
     return stats;
 }
-void
-increment_entries(branch_stats* stats)
-{
-    ++stats->reads;
-}
 
+/**
+ * Increments the total reads of the branch_status object
+ * and updates any mispredictions and the rate if needed
+ **/
+void
+update_statistics(branch_stats* stats,int correct)
+{
+    if(!correct)
+        ++stats->mispredictions;
+    ++stats->reads;
+    stats->misprediction_rate = (float)stats->mispredictions/(float)stats->reads * 100;
+}
+/**
+ * Prints out the reads and mispredictions from the branch_status object
+ **/
 void
 print_results(branch_stats* stats)
 {
-    printf("Read %zu entries\nMisprediction rate: %.2f%%\n",stats->reads,stats->misprediction_rate);
+    printf("Read %zu entries\nMisprediction rate: %.2f%%\n"
+            ,stats->reads,stats->misprediction_rate);
 }
+
 /**
  * Takes the Program Counter Address and extracts the bits that would
  *  be used to choose which PHT 
@@ -115,9 +130,7 @@ print_results(branch_stats* stats)
 unsigned
 extract_PC_bits(unsigned n,uint32_t PC)
 {
-    unsigned extracted_bits = 0;
-
-    return extracted_bits;
+    return (PC >> 2) & ((1 << n)-1);
 }
 
 /**
@@ -137,24 +150,96 @@ init_GAp_predictor(unsigned counter_size, unsigned GHR_size, unsigned PC_bits)
     GAp_predictor* predictor = malloc(sizeof(GAp_predictor));
     predictor->m = GHR_size;
     predictor->n = counter_size;
+    predictor->PC_bits = PC_bits;
     predictor->GHR = 0;
     
     // number of PHT is 2^m, where m is size of GHR entry
-    unsigned num_PHT = 1 << GHR_size;
+    unsigned num_PHT = 1 << (PC_bits );
     // number of entries is 2^p, where p is size of saturating counter
-    unsigned num_PHT_entry = 1 << counter_size;
+    unsigned num_PHT_entry = 1 << (GHR_size );
 
     // Create PHT and for each PHT create appropiate number of entries
-    predictor->phts = malloc(sizeof(saturating_counter)*num_PHT);
+    predictor->phts = malloc(sizeof(PHT)*num_PHT);
     for(ssize_t i = 0;i < num_PHT;++i)
     {
         predictor->phts[i].counters = malloc(sizeof(saturating_counter) * num_PHT_entry);
         // initialize all saturating counters to 00 (not taken)
-        for(ssize_t j =0; i < num_PHT_entry;++j)
+        for(ssize_t j =0; j < num_PHT_entry;++j)
             predictor->phts[i].counters[j].counter = 0;
     }
     return predictor;
 }
+
+/**
+ * Check if branch predictor chooses to take branch or not
+ *  by checking the selected PHT table determined by 
+ *  PC and the index of the GHR if saturating counter chooses 
+ *  taken or not taken
+ *
+ * param[predictor] GAp predictor object containing GHR, PHTs 
+ *  (containing saturating counter) 
+ * param[PHT_choose] extracted bits from the PC that choose which PHT from
+ *  the PHTs array of the GAp_predictor object member
+ **/
+int
+check_prediction(GAp_predictor* predictor,unsigned PHT_choose)
+{
+    int taken = 0;
+    //printf("GHR: %u\tPHT choose:%u\n",predictor->GHR,PHT_choose);
+    unsigned counter = predictor->phts[PHT_choose].counters[predictor->GHR].counter;
+    //by shifting n - 1, if result is 1, predict taken, otherwise its not taken
+    //MSB = 1 taken, MSB = 0 = not taken
+    //printf("counter: %u\n",counter);
+    taken = counter >> (predictor->n-1);
+    
+    return taken;
+}
+
+/**
+ * update the branch predictor to reflect branch outcome and predictor
+ *  results. 
+ * 1. update GHR
+ *      * shift left:
+ *          - if branch taken: shifted bit is 1
+ *          - else shifted bit is 0
+ *      * constantly mask overflowing bits incase left
+ *          shift surpasses bit amount
+ * 2. update PHT that was used
+ *      * increment/decrement saturating counter of that PHT
+ *      * check if counter exceeds maximum taken or if already 0
+ *          - mask number to prevent overflow
+ *          - if 0 and not taken, keep 0
+ *
+ * param[predictor] Branch predictor object containing GHR
+ *  that indexes PHT, which contains saturating counter
+ * param[correct] 1 if predictor was correct, 0 if predictor was not
+ * param[taken] 1 if branch was taken, 0 if branch was not taken
+ * param[PHT_choose] portion of PC that chooses which PHT to use in array
+ *  of PHT
+ **/
+void
+update_branch_predictor(GAp_predictor* predictor,int correct,
+        int taken, unsigned PHT_choose)
+{
+    if(taken)
+    {
+        ++predictor->phts[PHT_choose].counters[predictor->GHR].counter;
+
+        predictor->phts[PHT_choose].counters[predictor->GHR].counter &=
+            (1<<(predictor->n))-1;
+    }
+    else
+    {
+        // if not 0
+        if(predictor->phts[PHT_choose].counters[predictor->GHR].counter)
+            --predictor->phts[PHT_choose].counters[predictor->GHR].counter;
+    }
+    //shift bit, shift 1 if correct, mask overflow to 0
+    predictor->GHR <<= 1;
+    predictor->GHR |= (taken);
+    predictor->GHR &= (1<<(predictor->m))-1;
+}
+
 /**
  * Analyze the branch operation and analyze PC addresses of the 
  *  line from the trace file. FOR BASE this only will act on BCN
@@ -168,30 +253,49 @@ void
 analyze_trace(branch_stats* stats,GAp_predictor* predictor,
         char* line)
 {
-    /*
-     * FROM TRACE
-     * branch operation: BUC, BCN, CAL, RET
-     *  For base just look at BCN
-     * Current Program Counter
-     * NEXT Instruction Program Counter
-     */
+    /**
+    *  FROM TRACE
+    *  branch operation: BUC, BCN, CAL, RET
+    *   For base just look at BCN
+    *  Current Program Counter
+    *  NEXT Instruction Program Counter
+    **/
     ssize_t entries;
     char* branch_op = strsep(&line," ");
     uint32_t PC = atoi(strsep(&line," "));
     uint32_t PC_next = atoi(strsep(&line," "));
+    //unsigned table = extract_PC_bits(predictor->n,PC);
+    //printf("%x\n",PC);
+    unsigned PC_extracted = extract_PC_bits(predictor->PC_bits,PC);
+    //printf("Extracted: %x\n",extract_PC_bits(predictor->PC_bits,PC));
 
+    int taken = 0;
+    // if PC_next is not PC + 4, branch was taken
+    if((PC + 4) != PC_next)
+    {
+        taken = 1;
+    }
+    int prediction = 0;
+    int correct = 0;
     if(strcmp("BCN",branch_op)==0)
-        increment_entries(stats);
-
-
+    {
+        prediction = check_prediction(predictor, PC_extracted);
+        if(taken == prediction)
+        {
+            puts("Correct!");
+            correct = 1;
+        }
+        update_branch_predictor(predictor,correct,taken,PC_extracted);
+        update_statistics(stats,correct);
+    }
 }
 
-/* CLI INPUTS
- * benchmark: name of trace file
- * m: size of GHR in bytes
- * p: portion of PC that is used to index PHT excluding lower 2 bits
- * n: size of each saturating counter
- */
+/** CLI INPUTS
+ *  benchmark: name of trace file
+ *  m: size of GHR in bytes
+ *  p: portion of PC that is used to index PHT excluding lower 2 bits
+ *  n: size of each saturating counter
+ **/
 int
 main(int argc,char *argv[])
 {
